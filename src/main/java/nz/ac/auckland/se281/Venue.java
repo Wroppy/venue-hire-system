9c9efe5ba2d69e40d980f31f031d2907a9b8ea0d
Weyman Wong <wxwong806@gmail.com>
2024-03-19 22:34:42 +1300

    Changed attribute names and added getter methods for current attributes

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -1,15 +1,31 @@
 package nz.ac.auckland.se281;
 
 public class Venue {
-  private String venueName;
-  private String venueCode;
-  private Integer venueCapacity;
-  private Integer venueHireFee;
+  private String name;
+  private String code;
+  private Integer capacity;
+  private Integer hireFee;
 
   public Venue(String name, String code, Integer capacity, Integer hireFee) {
-    this.venueName = name;
-    this.venueCode = code;
-    this.venueCapacity = capacity;
-    this.venueHireFee = hireFee;
+    this.name = name;
+    this.code = code;
+    this.capacity = capacity;
+    this.hireFee = hireFee;
+  }
+
+  public String getCode() {
+    return this.code;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public Integer getCapcity() {
+    return this.capacity;
+  }
+
+  public Integer getHireFee(){
+    return this.hireFee;
   }
 }
