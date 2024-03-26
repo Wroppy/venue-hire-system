97d5bb01c05dab1abb88f01041e1921e42f56a7f
Weyman Wong <wxwong806@gmail.com>
2024-03-26 21:48:23 +1300

    implemented the set system date feature and reformatted code

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -2,13 +2,14 @@ package nz.ac.auckland.se281;
 
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
-
 import nz.ac.auckland.se281.MessageCli;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class VenueHireSystem {
   private ArrayList<Venue> venues;
+  private LocalDate date;
 
   public VenueHireSystem() {
     this.venues = new ArrayList<Venue>();
@@ -16,6 +17,7 @@ public class VenueHireSystem {
 
   private boolean isVenueCodeUnique(String code) {
     // Loops through each venue and checks if the code is in use
+    // Returns false if one exists, and true otherwise
     for (Venue venue : this.venues) {
       if (code.equals(venue.getCode())) {
         return false;
@@ -35,6 +37,7 @@ public class VenueHireSystem {
   }
 
   private boolean isInt(String s) {
+    // Returns whether a string is in an integer format
     try {
       Integer.parseInt(s);
       return true;
@@ -44,6 +47,7 @@ public class VenueHireSystem {
   }
 
   private String getIntString(int n) {
+    // Gets the string version of an integer
     String[] strings = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
     return strings[n - 1];
   }
@@ -51,7 +55,7 @@ public class VenueHireSystem {
   private void printVenuesHeading() {
     int totalVenues = this.venues.size();
 
-    // Accounts for 0 venues
+    // If no venues, tells the user to add venues
     if (totalVenues == 0) {
       MessageCli.NO_VENUES.printMessage();
       return;
@@ -67,20 +71,25 @@ public class VenueHireSystem {
       connector = "is";
     }
 
-    // For less than 10
+    // Sets the number of venue in either english or number form
     if (totalVenues < 10) {
       number = this.getIntString(totalVenues);
     } else {
       number = String.valueOf(totalVenues);
     }
 
+    // Prints the heading based on the number of venues in the system
     MessageCli.NUMBER_VENUES.printMessage(connector, number, plural);
   }
 
   public void printVenues() {
     this.printVenuesHeading();
 
-    String name, code, capacity, fee;
+    String name;
+    String code;
+    String capacity;
+    String fee;
+
     // Loops and prints out all venues
     for (Venue venue : this.venues) {
       name = venue.getName();
@@ -138,13 +147,22 @@ public class VenueHireSystem {
       return;
     }
     Venue venue = new Venue(venueName, venueCode, capacity, hireFee);
-    this.venues.add(venue);
+    this.venues.add(venue); // Adds to the system
 
     MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
   }
 
   public void setSystemDate(String dateInput) {
-    // TODO implement this method
+    // Splits the date string into its day, month and year components
+    String[] dateParams = dateInput.split("/");
+    int day = Integer.parseInt(dateParams[0]);
+    int month = Integer.parseInt(dateParams[1]);
+    int year = Integer.parseInt(dateParams[2]);
+
+    // Gets LocalDate instance of specified date
+    this.date = LocalDate.of(year, month, month);
+
+    MessageCli.DATE_SET.printMessage(dateInput);
   }
 
   public void printSystemDate() {
