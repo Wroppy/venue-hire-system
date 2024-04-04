e40032a95f48f274b9fad1d2d64ef4db84fe117f
Weyman Wong <wxwong806@gmail.com>
2024-04-04 17:45:06 +1300

    Added a constructor that accepts a string input in the form DD/MM/YYYY

--- src/main/java/nz/ac/auckland/se281/Date.java
+++ src/main/java/nz/ac/auckland/se281/Date.java
@@ -11,9 +11,21 @@ public class Date {
     this.year = year;
   }
 
+  public Date(String date) {
+    // Given a date in the form of DD/MM/YYYY, converts it the integer equivalent 
+    String[] dateOptions = date.split("/");
+    int day = Integer.parseInt(dateOptions[0]);
+    int month = Integer.parseInt(dateOptions[1]);
+    int year = Integer.parseInt(dateOptions[2]);
+
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
   @Override
   public String toString() {
-    // Makes sure the month is is the format of DD/MM/YYYY
+    // Returns string in the format of DD/MM/YYYY
     return String.format("%02d/%02d/%04d", this.day, this.month, this.year);
   }
 }
